9bfab70fd4f301fc1e5bd9511ab359b7e0174d66
Prasanna <github@prasansrini.com>
2024-05-06 14:21:02 +0200

    Initial package structure.

---
+++ app/src/main/java/com/my/notes/feature_note/di/NotesApplication.kt
@@ -0,0 +1,6 @@
+package com.my.notes.feature_note.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+class NotesApplication : Application()
--- app/src/main/java/com/my/notes/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/my/notes/feature_note/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.my.notes
+package com.my.notes.feature_note.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
