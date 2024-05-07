d053cedb0137ea9700f16255ef05e6918e911ba3
Prasanna <github@prasansrini.com>
2024-05-07 14:59:25 +0200

    Added implementation for MainActivity.

--- app/src/main/java/com/my/notes/feature_note/presentation/util/Screen.kt
+++ app/src/main/java/com/my/notes/feature_note/presentation/util/Screen.kt
@@ -1,6 +1,6 @@
 package com.my.notes.feature_note.presentation.util
 
 sealed class Screen(val route: String) {
-	data object NoteScreen : Screen("notes_screen")
-	data object AddEditNoteScreen : Screen("add_edit_note_screen")
+	public object NoteScreen : Screen("notes_screen")
+	public object AddEditNoteScreen : Screen("add_edit_note_screen")
 }
